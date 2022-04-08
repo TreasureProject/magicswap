b52c45a7fcb89d9f051b2364a6b6d07e9c729b6c
Yuya Oiwa <yuya.oiwa@me.com>
2022-04-08 12:11:23 +0900

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
