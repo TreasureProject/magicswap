f017366a6960b390edbca6768bba48b636585a50
Yuya Oiwa <yuya.oiwa@me.com>
2022-04-14 18:32:11 +0900

    mobile pool list select

--- app/routes/pools.tsx
+++ app/routes/pools.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { PlusSmIcon, SearchIcon } from "@heroicons/react/solid";
+import { PlusSmIcon, SearchIcon, XIcon } from "@heroicons/react/solid";
 import type { LoaderFunction } from "@remix-run/cloudflare";
 import { json } from "@remix-run/cloudflare";
 import type { ShouldReloadFunction } from "@remix-run/react";
@@ -8,6 +8,7 @@ import type { GetPairsQuery } from "~/graphql/generated";
 import { sdk } from "~/utils/api.server";
 import { getApr } from "~/utils/price";
 import cn from "clsx";
+import { Dialog, Transition } from "@headlessui/react";
 
 type LoaderData = {
   pairs: GetPairsQuery["pairs"];
@@ -29,6 +30,148 @@ export default function Pools() {
   const [mobileFiltersOpen, setMobileFiltersOpen] = React.useState(false);
   return (
     <div className="my-12 flex flex-1 flex-col">
+      <Transition.Root show={mobileFiltersOpen} as={React.Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 flex z-40 lg:hidden"
+          onClose={setMobileFiltersOpen}
+        >
+          <Transition.Child
+            as={React.Fragment}
+            enter="transition-opacity ease-linear duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-linear duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-50" />
+          </Transition.Child>
+
+          <Transition.Child
+            as={React.Fragment}
+            enter="transition ease-in-out duration-300 transform"
+            enterFrom="translate-x-full"
+            enterTo="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leaveFrom="translate-x-0"
+            leaveTo="translate-x-full"
+          >
+            <div className="ml-auto relative max-w-xs w-full h-full bg-gray-800 shadow-xl pt-4 flex flex-col overflow-y-auto">
+              <div className="px-4 flex items-center justify-between">
+                <h2 className="text-lg font-medium text-gray-200">
+                  Choose Pool
+                </h2>
+                <button
+                  className="-mr-2 w-10 h-10 p-2 flex items-center justify-center text-gray-400 hover:text-gray-500"
+                  onClick={() => setMobileFiltersOpen(false)}
+                >
+                  <span className="sr-only">Close menu</span>
+                  <XIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+              <div className="p-2">
+                <label htmlFor="liquidity-pools" className="sr-only">
+                  Liquidity Pool
+                </label>
+                <div className="relative mt-1 rounded-md shadow-sm">
+                  <input
+                    type="text"
+                    name="liquidity-pools"
+                    id="liquidity-pools"
+                    className="block w-full rounded-md border-gray-700 bg-gray-900 pr-10 focus:border-gray-500 focus:ring-gray-500 text-sm"
+                    placeholder="Search for liquidity pools"
+                  />
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                    <SearchIcon
+                      className="h-5 w-5 text-gray-700"
+                      aria-hidden="true"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="flex min-h-0 flex-1 flex-col mt-4">
+                <div className="sticky top-0 z-10 border-b-[0.5px] border-gray-600 px-6 py-2 text-sm font-medium text-gray-500 flex justify-between">
+                  <h3>Pools</h3>
+                  <h3>APR</h3>
+                </div>
+                <div className="flex-1 overflow-auto">
+                  <ul>
+                    {pairs.map((pair) => (
+                      <li key={pair.id}>
+                        <NavLink
+                          to={`/pools/${pair.id}/manage`}
+                          prefetch="intent"
+                          className="focus:outline-none"
+                        >
+                          {({ isActive }) => (
+                            <div
+                              className={cn(
+                                "px-6 py-5 flex items-center space-x-3 border-l-2 group",
+                                {
+                                  "border-red-600 text-red-600 bg-red-500/10":
+                                    isActive,
+                                  "hover:border-gray-300 border-transparent":
+                                    !isActive,
+                                }
+                              )}
+                            >
+                              <div className="flex justify-between w-full items-center">
+                                <div className="flex items-center space-x-4">
+                                  <div className="flex -space-x-4">
+                                    <img
+                                      src="https://via.placeholder.com/400"
+                                      alt="placeholder"
+                                      className={cn(
+                                        "w-8 h-8 rounded-full ring-1 z-10",
+                                        {
+                                          "ring-red-400": isActive,
+                                          "ring-gray-800": !isActive,
+                                        }
+                                      )}
+                                    />
+                                    <img
+                                      src="https://via.placeholder.com/400"
+                                      alt="placeholder"
+                                      className={cn(
+                                        "w-8 h-8 rounded-full ring-1",
+                                        {
+                                          "ring-red-400": isActive,
+                                          "ring-gray-800": !isActive,
+                                        }
+                                      )}
+                                    />
+                                  </div>
+                                  <p
+                                    className={cn("text-sm font-medium", {
+                                      "text-red-500": isActive,
+                                      "text-gray-400 group-hover:text-gray-200":
+                                        !isActive,
+                                    })}
+                                  >
+                                    {pair.name}
+                                  </p>
+                                </div>
+                                <p
+                                  className={cn("font-bold", {
+                                    "text-red-500": isActive,
+                                  })}
+                                >
+                                  {getApr(pair.volumeUSD, pair.reserveUSD)}%
+                                </p>
+                              </div>
+                            </div>
+                          )}
+                        </NavLink>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </Transition.Child>
+        </Dialog>
+      </Transition.Root>
       <h2 className="text-xl font-medium">Pools</h2>
       <aside>
         <h2 className="sr-only">Pool List</h2>
