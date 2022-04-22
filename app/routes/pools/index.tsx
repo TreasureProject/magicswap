fe698d9f0d4ea6237381a72cc25c77c51f4f7a07
Yuya Oiwa <yuya.oiwa@me.com>
2022-04-22 12:46:15 +0900

    move catch boundary to analytics and manange

--- app/routes/pools/index.tsx
+++ app/routes/pools/index.tsx
@@ -15,18 +15,3 @@ export default function Manage() {
     </div>
   );
 }
-
-export function CatchBoundary() {
-  const caught = useCatch();
-  const params = useParams();
-  if (caught.status === 404) {
-    return (
-      <div className="flex h-full flex-col items-center justify-center">
-        <p className="text-[0.6rem] text-gray-500 sm:text-base">
-          {params.poolId} not found.
-        </p>
-      </div>
-    );
-  }
-  throw new Error(`Unhandled error: ${caught.status}`);
-}
