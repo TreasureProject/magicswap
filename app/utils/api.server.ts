a8a2be51b942e38c1f175aa45c51bc5e7f85f6c0
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2022-04-25 22:27:04 -0700

    fix build

--- app/utils/api.server.ts
+++ app/utils/api.server.ts
@@ -1,14 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 
-import { getSdk as getBlocksSdk } from "~/graphql/blocks.generated";
 import { getSdk as getExchangeSdk } from "~/graphql/exchange.generated";
-
-export const blocksSdk = getBlocksSdk(
-  new GraphQLClient(
-    "https://api.thegraph.com/subgraphs/name/sushiswap/arbitrum-blocks",
-    { fetch }
-  )
-);
 
 export const exchangeSdk = getExchangeSdk(
   new GraphQLClient(
