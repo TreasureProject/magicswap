cab984885ca012debba0c943e0b2196eb4305c50
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2022-05-01 20:05:33 -0700

    add token approval processes

---
+++ app/hooks/useApproval.ts
@@ -0,0 +1,35 @@
+import { BigNumber, utils } from "ethers";
+import { MaxUint256 } from "@ethersproject/constants";
+import { erc20ABI, useAccount, useContractRead } from "wagmi";
+import type { Token } from "~/types";
+import { useContractWrite } from "./useContractWrite";
+
+export const useApproval = (token: Token) => {
+  const contractConfig = {
+    addressOrName: token.id,
+    contractInterface: erc20ABI,
+  };
+
+  const { data: accountData } = useAccount();
+  const { data: allowance } = useContractRead(contractConfig, "allowance", {
+    args: [accountData?.address, "0x0a073b830cd4247d518c4f0d1bafd6edf7af507b"],
+    enabled: !!accountData?.address,
+  });
+
+  const { write: writeApprove } = useContractWrite(contractConfig, "approve");
+  return {
+    isApproved: allowance
+      ? BigNumber.from(utils.formatEther(allowance)) >= MaxUint256
+      : false,
+    approve: () =>
+      writeApprove(
+        {
+          args: [
+            "0x0a073b830cd4247d518c4f0d1bafd6edf7af507b",
+            MaxUint256.toString(),
+          ],
+        },
+        `Approve ${token.symbol}`
+      ),
+  };
+};
