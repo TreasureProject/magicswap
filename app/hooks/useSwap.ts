038e5011b410c2635073475c0609258fbea1de3e
Yuya Oiwa <yuya.oiwa@me.com>
2022-05-06 09:33:11 +0900

    extract confirm modal as a component

--- app/hooks/useSwap.ts
+++ app/hooks/useSwap.ts
@@ -13,32 +13,44 @@ export const useSwap = () => {
   const { slippage, deadline } = useSettings();
   const statusRef = useRef<Optional<string>>(undefined);
 
-  const { write: writeSwapEthForExactTokens } = useV2RouterWrite(
-    "swapETHForExactTokens",
-    statusRef.current
-  );
-  const { write: writeSwapExactEthForTokens } = useV2RouterWrite(
-    "swapExactETHForTokens",
-    statusRef.current
-  );
-  const { write: writeSwapExactTokensForEth } = useV2RouterWrite(
-    "swapExactTokensForETH",
-    statusRef.current
-  );
-  const { write: writeSwapTokensForExactEth } = useV2RouterWrite(
-    "swapTokensForExactETH",
-    statusRef.current
-  );
-  const { write: writeSwapExactTokensForTokens } = useV2RouterWrite(
-    "swapExactTokensForTokens",
-    statusRef.current
-  );
-  const { write: writeSwapTokensForExactTokens } = useV2RouterWrite(
-    "swapTokensForExactTokens",
-    statusRef.current
-  );
+  const {
+    write: writeSwapEthForExactTokens,
+    isError: isError1,
+    isSuccess: isSuccess1,
+    isLoading: isLoading1,
+  } = useV2RouterWrite("swapETHForExactTokens", statusRef.current);
+  const {
+    write: writeSwapExactEthForTokens,
+    isError: isError2,
+    isSuccess: isSuccess2,
+    isLoading: isLoading2,
+  } = useV2RouterWrite("swapExactETHForTokens", statusRef.current);
+  const {
+    write: writeSwapExactTokensForEth,
+    isError: isError3,
+    isSuccess: isSuccess3,
+    isLoading: isLoading3,
+  } = useV2RouterWrite("swapExactTokensForETH", statusRef.current);
+  const {
+    write: writeSwapTokensForExactEth,
+    isError: isError4,
+    isSuccess: isSuccess4,
+    isLoading: isLoading4,
+  } = useV2RouterWrite("swapTokensForExactETH", statusRef.current);
+  const {
+    write: writeSwapExactTokensForTokens,
+    isError: isError5,
+    isSuccess: isSuccess5,
+    isLoading: isLoading5,
+  } = useV2RouterWrite("swapExactTokensForTokens", statusRef.current);
+  const {
+    write: writeSwapTokensForExactTokens,
+    isError: isError6,
+    isSuccess: isSuccess6,
+    isLoading: isLoading6,
+  } = useV2RouterWrite("swapTokensForExactTokens", statusRef.current);
 
-  return (
+  const swap = (
     inputToken: Token,
     outputToken: Token,
     rawAmountIn: number,
@@ -161,5 +173,25 @@ export const useSwap = () => {
         );
       }
     }
+  };
+
+  return {
+    swap,
+    isLoading:
+      isLoading1 ||
+      isLoading2 ||
+      isLoading3 ||
+      isLoading4 ||
+      isLoading5 ||
+      isLoading6,
+    isError:
+      isError1 || isError2 || isError3 || isError4 || isError5 || isError6,
+    isSuccess:
+      isSuccess1 ||
+      isSuccess2 ||
+      isSuccess3 ||
+      isSuccess4 ||
+      isSuccess5 ||
+      isSuccess6,
   };
 };
