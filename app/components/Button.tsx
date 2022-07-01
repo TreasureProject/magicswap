29b38a9280a99c37305d97ed12f187faf885ba12
jc <jcheese@treasure.lol>
2022-07-01 14:00:12 +0900

    color changes

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -7,7 +7,7 @@ export const Button = ({ className, ...buttonProps }: Props) => {
   return (
     <button
       className={clsx(
-        "inline-flex w-full items-center justify-center rounded border border-transparent bg-ruby-600 px-2.5 py-3 text-xs font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-ruby-500 focus:ring-offset-2 sm:text-lg",
+        "inline-flex w-full items-center justify-center rounded border border-transparent bg-ruby-900 px-2.5 py-3 text-xs font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-ruby-500 focus:ring-offset-2 sm:text-lg",
         className,
         disabled ? "opacity-50" : "hover:bg-ruby-700"
       )}
