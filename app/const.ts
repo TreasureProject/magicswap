aeae9cda53b1fb860c6550e6824381b3770c6d34
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2022-11-21 15:08:22 -0800

    add magic-gfly mainnet pair

--- app/const.ts
+++ app/const.ts
@@ -10,7 +10,7 @@ export const CONTRACT_ADDRESSES: Record<number, Record<AppContract, string>> = {
   [chainId.arbitrum]: {
     [AppContract.Router]: "0x23805449f91bb2d2054d9ba288fdc8f09b5eac79",
     [AppContract.MagicElmPair]: "0xf904469497e6a179a9d47a7b468e4be42ec56e65",
-    [AppContract.MagicGflyPair]: "",
+    [AppContract.MagicGflyPair]: "0x088f2bd3667f385427d9289c28725d43d4b74ab4",
   },
   [chainId.arbitrumGoerli]: {
     [AppContract.Router]: "0xe6ef3dac2ba5b785a36c2200da2c087735c3b426",
