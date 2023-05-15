bdd5952ee783becd66fe2ce891eda8fb55c68cb7
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2023-05-15 11:44:12 -0700

    whitelist anima pair

--- app/const.ts
+++ app/const.ts
@@ -4,9 +4,13 @@ export enum AppContract {
   Router,
   // MagicElmPair,
   MagicGflyPair,
+  MagicAnimaPair,
 }
 
-export const CONTRACT_ADDRESSES: Record<number, Record<AppContract, string>> = {
+export const CONTRACT_ADDRESSES: Record<
+  number,
+  Partial<Record<AppContract, string>>
+> = {
   [chainId.arbitrum]: {
     [AppContract.Router]: "0x23805449f91bb2d2054d9ba288fdc8f09b5eac79",
     // [AppContract.MagicElmPair]: "0xf904469497e6a179a9d47a7b468e4be42ec56e65",
@@ -16,6 +20,7 @@ export const CONTRACT_ADDRESSES: Record<number, Record<AppContract, string>> = {
     [AppContract.Router]: "0xe6ef3dac2ba5b785a36c2200da2c087735c3b426",
     // [AppContract.MagicElmPair]: "0x82b79579f07a3539f10d0b2c35316b0e8333b2cc",
     [AppContract.MagicGflyPair]: "0x7e8ce14d9d541b3494e20fba97ddd010f29b0250",
+    [AppContract.MagicAnimaPair]: "0xa8e16376fec06f7f921c0ef9253902494631ae7e",
   },
 };
 
