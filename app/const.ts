1f09ead8ddd8244c78a1963de977ee8be9191265
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2023-06-27 14:47:02 -0700

    fix coinbase wallet; add elm testnet; add staging deployment ci

--- app/const.ts
+++ app/const.ts
@@ -4,7 +4,7 @@ import type { AddressString } from "./types";
 
 export enum AppContract {
   Router,
-  // MagicElmPair,
+  MagicElmPair,
   MagicGflyPair,
   MagicVeePair,
   MagicAnimaPair,
@@ -23,7 +23,7 @@ export const CONTRACT_ADDRESSES: Record<
   },
   [arbitrumGoerli.id]: {
     [AppContract.Router]: "0xe6ef3dac2ba5b785a36c2200da2c087735c3b426",
-    // [AppContract.MagicElmPair]: "0x82b79579f07a3539f10d0b2c35316b0e8333b2cc",
+    [AppContract.MagicElmPair]: "0xc175926f79c3f77efd2a88330aabe45f9066b617",
     [AppContract.MagicGflyPair]: "0x7e8ce14d9d541b3494e20fba97ddd010f29b0250",
     [AppContract.MagicVeePair]: "0xa5f4441c1dd3515767a4e33bacc320fb3828688f",
     [AppContract.MagicAnimaPair]: "0x7cfc374cfe753c9b77b6dac1d5d8c97ed84adc36",
