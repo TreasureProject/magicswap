51e6406ffd2f2d2176254c45672ae747ef45c2d8
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2023-12-23 11:52:09 -0800

    upgrade dependencies

--- test/setup-test-env.ts
+++
@@ -1,7 +0,0 @@
-import { installGlobals } from "@remix-run/node";
-import matchers from "@testing-library/jest-dom/matchers";
-import { expect } from "vitest";
-
-expect.extend(matchers);
-
-installGlobals();
