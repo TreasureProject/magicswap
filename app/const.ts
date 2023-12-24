d142cc03383db19422158e1c8507fddc718e58f0
0xkarel <95452264+karelvuong@users.noreply.github.com>
2023-12-24 16:01:35 -0500

    Add Smolcoin pair

--- app/const.ts
+++ app/const.ts
@@ -21,7 +21,7 @@ export const CONTRACT_ADDRESSES: Record<
     [AppContract.MagicGflyPair]: "0x088f2bd3667f385427d9289c28725d43d4b74ab4",
     [AppContract.MagicVeePair]: "0x6210775833732f144058713c9b36de09afd1ca3b",
     [AppContract.MagicAnimaPair]: "0x7bc27907ac638dbceb74b1fb02fc154da3e15334",
-    [AppContract.MagicSmolPair]: "0x0",
+    [AppContract.MagicSmolPair]: "0x33f4668f5a9a36514d85657e699569dbda3d77f1",
   },
   [arbitrumGoerli.id]: {
     [AppContract.Router]: "0xe6ef3dac2ba5b785a36c2200da2c087735c3b426",
@@ -29,7 +29,7 @@ export const CONTRACT_ADDRESSES: Record<
     [AppContract.MagicGflyPair]: "0x7e8ce14d9d541b3494e20fba97ddd010f29b0250",
     [AppContract.MagicVeePair]: "0xa5f4441c1dd3515767a4e33bacc320fb3828688f",
     [AppContract.MagicAnimaPair]: "0x7cfc374cfe753c9b77b6dac1d5d8c97ed84adc36",
-    [AppContract.MagicSmolPair]: "0x0",
+    [AppContract.MagicSmolPair]: "0x33f4668f5a9a36514d85657e699569dbda3d77f1",
   },
 };
 
