95f5117705b5abd2af018b94f03d2202de5f9ff1
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-03-04 16:45:23 -0800

    switch to thirdweb rpc

--- env.d.ts
+++ env.d.ts
@@ -2,7 +2,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE__ALCHEMY_KEY: string;
+  readonly VITE_THIRDWEB_CLIENT_ID: string;
   readonly VITE_ENABLE_TESTNETS: string;
   readonly VITE_WALLETCONNECT_PROJECT_ID: string;
 }
